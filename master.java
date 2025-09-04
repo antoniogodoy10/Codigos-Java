b9f3f4ee355cfc6ca50be913351f1e61e0208f2b
antoniogodoy10 <godoyantonio735@gmail.com>
2025-09-04 17:14:53 -0300

    Videoaula Java

---
+++ master.java
@@ -0,0 +1,16 @@
+public class master {
+
+    public static void main(String[] args) {
+
+       int colecaodeInteiros[] = {1,2,3,4,5};
+
+       System.out.println(colecaodeInteiros[3]);
+
+    }
+    
+
+   
+
+    
+    
+}
