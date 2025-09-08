c6e3a3233eb29e572652ddf96087d46861dfbf4e
antoniogodoy10 <godoyantonio735@gmail.com>
2025-09-08 19:00:41 -0300

    Soma Java

--- master.java
+++ master.java
@@ -6,10 +6,15 @@ public class master {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
 
-      System.out.println("Digite seu nome:");
-      String nome = sc.nextLine();
+      System.out.println("Digite o primeiro numero: ");
+      int n1 = sc.nextInt();
 
-      System.out.println("Olá, " + nome + "!");
+      System.out.println("Digite o segundo numero: ");
+      int n2 = sc.nextInt();
+
+      int soma = n1 + n2;
+
+      System.out.println("O resultado da soma eh " + soma);
 
     }
 
