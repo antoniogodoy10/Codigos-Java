c932e447969c9642b4e5b4e40a791bd84f464a30
antoniogodoy10 <godoyantonio735@gmail.com>
2025-09-08 19:04:19 -0300

    Idade

--- master.java
+++ master.java
@@ -6,15 +6,12 @@ public class master {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
 
-      System.out.println("Digite a base do triangulo: ");
-      int n1 = sc.nextInt();
+      System.out.println("Digite seu ano de nascimento: ");
+      int anonasc = sc.nextInt();
 
-      System.out.println("Digite a altura do triangulo: ");
-      int n2 = sc.nextInt();
+      int idade = 2025 - anonasc;
 
-      int triangulo = n1 * n2;
-
-      System.out.println("O resultado da soma eh " + triangulo);
+      System.out.println("Sua idade eh " + idade);
 
     }
 
