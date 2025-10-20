86af4b4fbc70981b2e1904e03449345cb8f7b81e
antoniogodoy10 <godoyantonio735@gmail.com>
2025-10-19 23:26:29 -0300

    Exercicio

--- Main.java
+++ Main.java
@@ -2,32 +2,32 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static int contarMaiuculas(String s)
+    public static int contarVogais(String s)
     {
-        int contador = 0;
-       for (int i = 0; i < s.length(); i++)
-       {
-              char c = s.charAt(i);
-              if (c >= 'A' && c <= 'Z')
-              {
-                contador++;
-              }
-       }
-
-       return contador;
-
-    }
-
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        String linha = sc.nextLine();
-
-        while (!linha.equals("FIM"))
+        int cont = 0;
+        for (int i = 0; i < s.length(); i++)
         {
-            int qtd = contarMaiuculas(linha);
-            System.out.println(qtd);
-            linha = sc.nextLine();
+            char c = s.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+            {
+                cont++;
+            }
         }
+
+        return cont;
     }
+   public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    String linha = sc.nextLine();
+
+    while (!linha.equals("FIM"))
+    {
+        int qtd = contarVogais(linha);
+        System.out.println(qtd);
+        linha = sc.nextLine();
+    }
+   }
+
 }
