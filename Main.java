064416fe1b4ed186806014636b70a192b7fff899
antoniogodoy10 <godoyantonio735@gmail.com>
2025-10-21 16:26:51 -0300

    Exercicio lab 02 github

--- Main.java
+++ Main.java
@@ -2,32 +2,28 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static int contarVogais(String s)
-    {
-        int cont = 0;
-        for (int i = 0; i < s.length(); i++)
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String normal = "";
+        String espelho = "";
+
+        int inicio = sc.nextInt();
+        int fim = sc.nextInt();
+
+
+        for (int i = inicio; i <= fim; i++)
         {
-            char c = s.charAt(i);
-            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-                c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
-            {
-                cont++;
-            }
+            normal = normal + i;
         }
 
-        return cont;
-    }
-   public static void main(String[] args) {
-    
-    Scanner sc = new Scanner(System.in);
-    String linha = sc.nextLine();
+        for (int j = fim; j >= inicio; j--)
+        {
+            String num = String.valueOf(j);
+            String invertido = new StringBuilder(num).reverse().toString();
+            espelho = espelho + invertido;
+        }
 
-    while (!linha.equals("FIM"))
-    {
-        int qtd = contarVogais(linha);
-        System.out.println(qtd);
-        linha = sc.nextLine();
-    }
-   }
-
+        System.out.println(normal + espelho);
+}
 }
