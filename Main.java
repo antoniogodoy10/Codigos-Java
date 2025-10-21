1e096f60c5515d2c6d2379847237fac1639337ad
antoniogodoy10 <godoyantonio735@gmail.com>
2025-10-21 16:57:30 -0300

    Exercicio 3 lab correto

--- Main.java
+++ Main.java
@@ -3,17 +3,25 @@ import java.util.Scanner;
 public class Main
 {
    public static void main(String[] args) {
+
+      Scanner sc = new Scanner (System.in);
     
       System.out.println("Digite uma frase: ");
       String frase = sc.nextLine();
 
       String[] palavras = frase.split(" ");
 
+      String resultado = "";
+
       for (int i = palavras.length - 1; i >= 0; i--)
       {
-         String 
+         String teste = palavras[i];
+         String invertido = new StringBuilder(teste).reverse().toString();
+         resultado += invertido + " ";
+
       }
 
+      System.out.println(resultado);
 
    }
 }  
