987bad78668c1c2e6d9e9b2cb45298446703917e
antoniogodoy10 <godoyantonio735@gmail.com>
2025-10-22 16:10:10 -0300

    Exercicio lista

--- Main.java
+++ Main.java
@@ -1,49 +1,22 @@
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        // Enquanto houver casos de teste
-        while (sc.hasNextInt()) {
-            int N = sc.nextInt(); // número de competidores
+        ListaLinear lista = new ListaLinear(5);
 
-            int[] largada = new int[N];
-            int[] chegada = new int[N];
-            int[] posicaoChegada = new int[N + 1]; 
-            // usamos N+1 porque os competidores vão de 1 até N
+        lista.inserirFim(10);
+        lista.inserirFim(20);
+        lista.inserirInicio(5);
+        lista.inserir(15, 2);
 
-            // Leitura do grid de largada
-            for (int i = 0; i < N; i++) {
-                largada[i] = sc.nextInt();
-            }
+        lista.mostrar(); // [ 5 10 15 20 ]
 
-            // Leitura do grid de chegada
-            for (int i = 0; i < N; i++) {
-                chegada[i] = sc.nextInt();
-                posicaoChegada[chegada[i]] = i; 
-                // guarda a posição final de cada competidor
-            }
+        System.out.println("Remove início: " + lista.removerInicio());
+        System.out.println("Remove fim: " + lista.removerFim());
+        lista.mostrar(); // [ 10 15 ]
 
-            // Agora criamos o vetor 'ordem' com base nas posições de chegada
-            int[] ordem = new int[N];
-            for (int i = 0; i < N; i++) {
-                ordem[i] = posicaoChegada[largada[i]];
-            }
+        System.out.println("Pesquisar 15: " + lista.pesquisar(15));
+        System.out.println("Pesquisar 50: " + lista.pesquisar(50));
 
-            // Contar inversões (ultrapassagens)
-            int ultrapassagens = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = i + 1; j < N; j++) {
-                    if (ordem[i] > ordem[j]) {
-                        ultrapassagens++;
-                    }
-                }
-            }
-
-            System.out.println(ultrapassagens);
-        }
-
-        sc.close();
+        lista.remover(0);
+        lista.mostrar(); // [ 15 ]
     }
 }
