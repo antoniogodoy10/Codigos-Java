a35f4500f2368ab0d936fbf4b68b90e5d733e03a
antoniogodoy10 <godoyantonio735@gmail.com>
2026-03-14 19:13:28 -0300

    Ex 1 TP (correto)

--- Cripto.java
+++ Cripto.java
@@ -2,21 +2,22 @@ import java.util.Scanner;
 
 public class Cripto{
 
-    public static void Mudar(String str){
-     for(int i = 0; i < str.length; i++){
+    public static String Mudar(String str){
+       String novaString = " ";
+       for(int i = 0; i < str.length(); i++){
        char c = str.charAt(i);
-       str.charAt(i) = c + 3;
+       novaString = novaString + (char)(c + 3);
        }
-       return;
+       return novaString;
     }
     public static void main (String args[]) {
-
+    //declaração do scanner
     Scanner sc = new Scanner (System.in);
 
     String str = sc.nextLine();
 
     String resultado = Mudar(str);
-    
+    System.out.println(resultado);
     }
     
 }
