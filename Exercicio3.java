7f00300e85338c05018f6b367b1f12d97d1d3574
antoniogodoy10 <godoyantonio735@gmail.com>
2026-03-16 20:03:44 -0300

    Ex 3 TP 1

--- Exercicio3.java
+++ Exercicio3.java
@@ -1,16 +1,36 @@
 import java.util.*;
 public class Exercicio3
 {
+    //criação do metodo Inteiros
+    public static boolean Inteiros(String str)
+    {
+        for (int i = 0; i < str.length(); i++)
+        {
+            char c = str.charAt(i);
+            //condição para ver se o numero é inteiro
+            if (c >= '0' && c <= '9')
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    //criação do metodo Consoantes 
     public static boolean Consoantes(String str){
         for (int i = 0; i < str.length(); i++)
       {
         char c = str.charAt(i);
         //condição para ver se tem vogal
-       if (c == 'a' && c == 'e' && c == 'i' && c == 'o' && c == 'u' &&
-            c == 'A' && c == 'E' && c == 'I' && c == 'O' && c == 'U')
+       if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+            c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
         {
             return false;
         }
+        //condição para se tiver numeros, o metodo Consoantes retornar falso
+       if (c >= '0' && c <= '9')
+            {
+                return false;
+            }
       }
       return true;
     }
@@ -39,22 +59,32 @@ public class Exercicio3
         //condição para ver se o metodo Vogais retornou true ou false
         if (Vogais(str))
         {
-            System.out.println("SIM");
+            System.out.print("SIM ");
         }
 
         else
         {
-            System.out.println("NAO");
+            System.out.print("NAO ");
         }
-
+        //condição para ver se o metodo Consoantes retornou true ou false
         if (Consoantes(str))
         {
-            System.out.println("SIM");
+            System.out.print("SIM ");
         }
 
         else
         {
-            System.out.println("NAO");
+            System.out.print("NAO ");
+        }
+         //condição para ver se Inteiros retornou false ou true  
+        if (Inteiros(str))
+        {
+            System.out.print("SIM ");
+        }
+
+        else
+        {
+            System.out.print("NAO ");
         }
            str = sc.nextLine(); 
     }
