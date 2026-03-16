f585b962ce3cfd1f31624577d928f4a42d77aaac
antoniogodoy10 <godoyantonio735@gmail.com>
2026-03-16 19:17:10 -0300

    Tp 1 ex 3 (em andamento)

---
+++ Exercicio3.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Exercicio3
+{
+    //criação metodo Vogais
+    public static boolean Vogais(String str){
+        //percorre a string str
+      for (int i = 0; i < str.length(); i++)
+      {
+        char c = str.charAt(i);
+       if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u' &&
+            c != 'A' && c != 'E' && c != 'I' && c != 'O' && c != 'U')
+        {
+            return false;
+        }
+      }
+      return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+         String str = sc.nextLine();
+        
+        while (!str.equals("FIM")){
+        //condição para ver se o metodo Vogais retornou true ou false
+        if (Vogais(str))
+        {
+            System.out.println("SIM");
+        }
+
+        else
+        {
+            System.out.println("NAO");
+        }
+           str = sc.nextLine(); 
+    }
+}
+}
